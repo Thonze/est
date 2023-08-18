a1f00c95e6fa7a3f45d43c23b8d26cc9596010c4
Thonze <mosesthonze@gmail.com>
2023-08-18 18:34:11 +0100

    cheerbequest

--- src/app/comps/Navbar.tsx
+++ src/app/comps/Navbar.tsx
@@ -3,19 +3,30 @@ import Image from 'next/image'
 
 
 export default function Navbar() {
+
+  // const btn = document.getElementById('menu-btn')!
+  // const nav = document.getElementById('menu')!
+
+  // btn.addEventListener('click', () =>{
+  //     btn.classList.toggle('open')
+  //     nav.classList.toggle('flex')
+  //     nav.classList.toggle('hidden')
+  // })
   
   return (
     <nav className='relative container mx-auto p-6 border-b-4'>
       <div className='flex items-center justify-between'>      
         <div className="logo pt-2">
-        <img src="/logo.png" alt='Sk'   />
       </div>
-      <div className='hidden md:flex space-x-6'>
-      <a className='hover:text-orange-500' href="/">Home</a>
-      <a className='hover:text-orange-500' href="/about">About</a>
-      <a className='hover:text-orange-500' href="/massa">Make your order</a>
+      <div className='hidden md:flex space-x-12'>
+      <a className='hover:text-orange-500' href="/">search</a>
+      <a className='hover:text-orange-500' href="/about">Fund someone</a>
+      <a className='hover:text-orange-500 ' href="/massa">How it works</a>
+      <a className='hover:text-orange-500' href="/massa"><img className='lg-absolute ' src="/logo.png" alt='Sk'   /></a>
+      <a className='hover:text-orange-500' href="/massa">About us</a>
+      <a className='hover:text-orange-500' href="/massa">Blog</a>
       </div>
-      <a href='' className=' hidden md:block  p-3 px-6 pt-2 text-white bg-red-700 rounded-full
+      <a href='' className=' hidden md:block  p-3 px-6 pt-2 text-blue-500 border-slate border border-slate-500  rounded-full  ring-offset-4
       baseline hover:bg-red-400'>Get Started</a>
       {/* Hambuger */}
       <button id='menu-btn' className='block hamburger md:hidden
@@ -29,7 +40,7 @@ export default function Navbar() {
      
       </div>
        {/* mobile menu */}
-      <div className='md:hidden'>
+      <div className='md:hidden' role='menubar'>
         <div id='menu' className='absolute flex flex-col items-center self-end hidden py-8
         mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6
         drop-shadow-md'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,125 @@ export default function Home() {
     <>
     <Navbar/>
     <main className="">
-      
       <section id='hero'>
-        <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 md:flex-row ">
-          <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
-            <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left" >
-            Shola's Kitchen
-            </h1>
-              <p className=' max-w-sm text-center text-red-400 md:text-left'
-                >Welcome to our restaurant web app! We are excited to bring you a 
-                new way to experience dining. With our app, you can easily browse 
-                our menu, place your order, and have it delivered directly to your 
-                doorstep or ready for pickup at our restaurant
-                </p>
-                <div className="flex justify-center md:justify-start">
-                <a href='' className='md:block  p-3 px-6 pt-2 text-white bg-red-700 rounded-full
-                  baseline hover:bg-red-400'>Get Started</a>
-                </div>
+        <div className="container mx-auto ">
             </div>
              {/* Image */}
-             <div className='md:w-1/2'>
-                <img src="/image.png"  alt='image'/>
+             <div className='brightness-50'>
+                <img src="/banner.png"  alt='image'/>
+                <div className="absolute inset-0 bg-black bg-opacity-50 text-black p-4">
+                {/* <h1 className="text-2xl font-bold">My Title</h1> */}
+                <p className="py-6 font-thin text-white text-md ">Cheerbequest</p>
+                <h3 className="m-6  py-6 font-bold text-3xl text-white text-md ">Raise Funds for that project you want to Achieve</h3>
+                <button className='text-white text-xl p-3  mx-3 outline outline-2 bg-sky-700'>Start a Cheerbequest</button>
+                <button className='text-white text-xl p-3 outline mx-3 outline-slate-300'>See how it Works</button>
+                <p className='text-white text-sm py-6 mx-3'>With a few easy steps , raise funds for a motive today</p>
+              </div>
+                
                 </div>
+        </section>
+
+        <section className='container mx-auto py-3 my-6 px-3 bg-slate-500'>
+          <div className='grid lg:grid-cols-2 md:grid-cols-2'>
+          <div className=' py-6 col-span-1 '>
+          <h1 className='text-2xl text-white  py-6 pt-3'>Start a Fundraiser in three steps</h1>
+          <img src='Funds.png' alt='image' className=' ps-14'/>
+          </div>
+           <div className='  col-span-1 text-white pt-20 '>
+            <p className=' '>Start your fundraiser</p>
+            <span className='text-xs my-6 py-3'>By creating an account, you can have access to create a fundraiser for a cause</span>
+            <p className=' '>Share your fundraiser</p>
+            <span className='text-xs py-6'>Share fundraiser with family and friends to meet fundraiser target</span>
+            <p className=' '>Withdraw your fundraiser</p>
+            <span className='text-xs'>Withdraw funds raise eaily after each fundraiser is completed</span>
+           </div>
+          </div>
+
+        </section>
+
+        <section className='container mx-auto py-3 my-6 px-3'>
+          <div className='grid lg:grid-cols-2 md:grid-cols-2'>
+            <div className='py-6 col-span-1'>
+              <h1 className='text-2xl text-black-100  py-6 pt-3 font-bold'>Raise Funds for</h1>
+            </div>
           </div>
         </section>
+
+        <section className='container mx-auto py-3 my-6 px-3  bg-slate-950 '>
+          <div className='flex'>
+          <h4 className=' grow text-white font-semibold'>Topic of interest</h4>  <span className='text-white flex flex-col flex-none'>see all</span>
+          </div>
+          <div className='h-screen p-4 Parent'>
+            <img src='' alt='' />
+            <div className='grid lg:grid-cols-3 md:grid-cols-2'>
+           <div className='col-span-1 py-3 bg-white mx-3 p-3'>
+           <img src='' alt='Image' />
+              <h4> Any mechanical Keyboard ethuisaist in design</h4>
+              <button className='px-3 py-2 rounded-lg cursor-pointer  bg-cyan-500'>see more</button>
+            </div>
+           <div className='col-span-1 py-3 bg-white mx-3 p-3'>
+           <img src='' alt='Image' />
+              <h4> Any mechanical Keyboard ethuisaist in design</h4>
+              <button className='px-3 py-2 rounded-lg cursor-pointer  bg-cyan-500'>see more</button>
+            </div>
+           <div className='col-span-1 py-3 bg-white mx-3 p-3'>
+           <img src='' alt='Image' />
+              <h4 className='p-3'> Any mechanical Keyboard ethuisaist in design</h4>
+              <button className='p-3 rounded-lg cursor-pointer  bg-cyan-500'>see more</button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className='container mx-auto py-3 my-6 px-3 '>
+          <div className='py-6'>
+            <h2 className='text-lg font-bold'> Fund someone</h2>
+          </div>
+          <div className=' grid lg:grid-cols-3 '>
+            <div className='col-span-1 px-3 mx-3'>
+              <img src= "" alt=''/>
+              <h4 className='text-lg font-semibold text-blue-700'> Help Devita beat cancer</h4>
+              <p>From supscription to gym classes to luxury classes</p>
+              <div className='bg-blue outline outline-1   rounded-sm my-3'>
+                <span className=' bg-white outline outline-1 rounded-sm'></span>
+              </div>
+              <div className=' py-3'><span className=' text-slate-950 font-medium '># 800, 000 raised </span> <span className='ml-6 text-sm'>60 donations</span></div>
+            </div>
+            <div className='col-span-1 px-3 mx-3'>
+              <img src= "" alt=''/>
+              <h4 className='text-lg font-semibold text-blue-700'> Help Father and his 20year's old son</h4>
+              <p>From supscription to gym classes to luxury classes</p>
+              <div className='bg-blue outline outline-1   rounded-sm my-3'>
+                <span className=' bg-white outline outline-1 rounded-sm'></span>
+              </div>
+              <div className=' py-3'><span className=' text-slate-950 font-medium '># 800, 000 raised </span> <span className='ml-6 text-sm'>60 donations</span></div>
+            </div>
+            <div className='col-span-1 px-3 mx-3'>
+              <img src= "" alt=''/>
+              <h4 className='text-lg font-semibold text-blue-700'> Community bridge</h4>
+              <p>From supscription to gym classes to luxury classes</p>
+              <div className='bg-blue outline outline-1   rounded-sm my-3'>
+                <span className=' bg-white outline outline-1 rounded-sm'></span>
+              </div>
+              <div className=' py-3'><span className=' text-slate-950 font-medium '># 800, 000 raised </span> <span className='ml-6 text-sm'>60 donations</span></div>
+            </div>
+          </div>
+        </section>
+
+        <section className='container mx-auto py-3 my-6 px-3  bg-slate-900 rounded-md '>
+          <div className='p-3 text-white'>
+            <div className='flex'>
+            <h4 className=' font-bold text-lg flex grow'>Ready to get started?</h4> <button className='flex flex-col bg-white flex-none text-black p-3 rounded-md outline-sky-700'>start a fundraiser</button>
+            </div>
+            <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit!</p>
+
+          </div>
+        </section>
+
+
+        <footer>
+          
+        </footer>
     </main>
     </>
   )
